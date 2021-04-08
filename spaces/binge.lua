b80f07fc8b500ade07fe268f258d59ed359e2873
selfire1 <try2banonym@gmail.com>
2021-04-08 17:53:25 +0200

    trying to get another chooser to work

--- spaces/binge.lua
+++ spaces/binge.lua
@@ -5,23 +5,43 @@ table.insert(Config.spaces, {
   togglProj = Config.projects.binge,
   togglDescr = "Binge",
   whitelist = {'video', 'browser'},
-  funcs = 'binge',
-  intentRequired = true,
-  intentSuggestions = {{ 
-    text = "Netflix"
-  },
-  {
-    text = "YouTube"
-  }}
+  funcs = 'binge'
 })
 
+-- Previous function
+-- Config.funcs.binge = {
+--   setup = function()
+--     -- Open YouTube with Brave
+--     hs.urlevent.openURLWithBundle("https://www.youtube.com/feed/subscriptions", "com.brave.Browser")
+--   end,
+--   teardown = function()
+--     -- Close YouTube und Netflix tabs
+--     Brave.killTabsByDomain("youtube.com|netflix.com")
+--   end
+-- }
+
+-- Trying having another chooser
 Config.funcs.binge = {
-  setup = function()
-    -- Open YouTube with Brave
-    hs.urlevent.openURLWithBundle("https://www.youtube.com/feed/subscriptions", "com.brave.Browser")
-  end,
-  teardown = function()
-    -- Close YouTube und Netflix tabs
-    Brave.killTabsByDomain("youtube.com|netflix.com")
+  setup = function ()
+    
+    hs.chooser.new(function(choice)
+    hs.urlevent.openURL(choice.url)
+    spoon.Headspace.startTimer(hs.settings.get("secrets").toggl.binge, choice.text)
+    end)
+    :choices({
+      {
+        text = 'Netflix',
+        url = 'http://netflix.com/'
+      },
+      {
+        text = 'Youtube',
+        url = 'https://www.youtube.com/feed/subscriptions/'
+      },
+    })
+    :placeholderText("What do you want to binge?")
+    :show()
+
   end
 }
+
+
