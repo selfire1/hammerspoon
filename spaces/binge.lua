085481d27fa9a9f037338d618d843c666a485c2c
selfire1 <try2banonym@gmail.com>
2021-04-09 10:13:06 +0200

    trying to get a timer to start

--- spaces/binge.lua
+++ spaces/binge.lua
@@ -2,31 +2,26 @@ table.insert(Config.spaces, {
   text = "Binge",
   subText = "Grab some popcorn and enjoy watching videos.",
   image = hs.image.imageFromAppBundle('org.epichrome.eng.YouTube'),
-  togglProj = Config.projects.binge,
-  togglDescr = "Binge",
+  -- togglProj = Config.projects.binge,
   whitelist = {'video', 'browser'},
   funcs = 'binge'
 })
 
--- Previous function
--- Config.funcs.binge = {
---   setup = function()
---     -- Open YouTube with Brave
---     hs.urlevent.openURLWithBundle("https://www.youtube.com/feed/subscriptions", "com.brave.Browser")
---   end,
---   teardown = function()
---     -- Close YouTube und Netflix tabs
---     Brave.killTabsByDomain("youtube.com|netflix.com")
---   end
--- }
-
 -- Trying having another chooser
+
 Config.funcs.binge = {
   setup = function ()
     
     hs.chooser.new(function(choice)
-    hs.urlevent.openURL(choice.url)
-    spoon.Headspace.startTimer(hs.settings.get("secrets").toggl.binge, choice.text)
+      -- Trying to get starting timer to work. Different attempts:
+      -- spoon.Headspace.startTimer(hs.settings.get("secrets").toggl.projects.binge, choice.text)
+      -- local toggl = dofile(hs.spoons.resourcePath('Headspace.spoon/toggl.lua'))
+      -- spoon.Headspace.startTimer(hs.settings.get("secrets").toggl.projects.binge, 'test')
+      local toggl = require('Spoons/Headspace/toggl')
+      toggl.startTimer(hs.settings.get("secrets").toggl.projects.binge, choice.text)
+
+      -- Open URL of choice
+      hs.urlevent.openURL(choice.url)
     end)
     :choices({
       {
@@ -39,8 +34,12 @@ Config.funcs.binge = {
       },
     })
     :placeholderText("What do you want to binge?")
-    :show()
-
+      :show()
+      
+  end,
+  teardown = function()
+    -- Close YouTube und Netflix tabs
+    Brave.killTabsByDomain("youtube.com|netflix.com")
   end
 }
 
