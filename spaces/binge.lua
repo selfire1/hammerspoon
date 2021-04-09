4ee142edf36184b624e3d3d35e06c53e0a8d1ba6
selfire1 <try2banonym@gmail.com>
2021-04-09 10:19:00 +0200

    moving chooser experiments to seperate branche

--- spaces/binge.lua
+++ spaces/binge.lua
@@ -8,40 +8,13 @@ table.insert(Config.spaces, {
   funcs = 'binge'
 })
 
--- Previous function
--- Config.funcs.binge = {
---   setup = function()
---     -- Open YouTube with Brave
---     hs.urlevent.openURLWithBundle("https://www.youtube.com/feed/subscriptions", "com.brave.Browser")
---   end,
---   teardown = function()
---     -- Close YouTube und Netflix tabs
---     Brave.killTabsByDomain("youtube.com|netflix.com")
---   end
--- }
-
--- Trying having another chooser
 Config.funcs.binge = {
-  setup = function ()
-    
-    hs.chooser.new(function(choice)
-    hs.urlevent.openURL(choice.url)
-    spoon.Headspace.startTimer(hs.settings.get("secrets").toggl.binge, choice.text)
-    end)
-    :choices({
-      {
-        text = 'Netflix',
-        url = 'http://netflix.com/'
-      },
-      {
-        text = 'Youtube',
-        url = 'https://www.youtube.com/feed/subscriptions/'
-      },
-    })
-    :placeholderText("What do you want to binge?")
-    :show()
-
+  setup = function()
+    -- Open YouTube with Brave
+    hs.urlevent.openURLWithBundle("https://www.youtube.com/feed/subscriptions", "com.brave.Browser")
+  end,
+  teardown = function()
+    -- Close YouTube und Netflix tabs
+    Brave.killTabsByDomain("youtube.com|netflix.com")
   end
 }
-
-
