f45fb379846f0b3c187b4558948517e81de8eced
Joschua <70809675+selfire1@users.noreply.github.com>
2024-02-09 12:07:09 +1000

    formatting

--- movewindows.lua
+++ movewindows.lua
@@ -1,34 +1,29 @@
 local movewindows = {}
 
-movewindows.start = function ()
-    
-    
-    -- Keybindings
-    local ctrl_cmd = {"cmd", "ctrl"}
-    local cmd_alt = {"cmd", "alt"}
-    local shift_ctrl_cmd = {"shift", "cmd", "ctrl"}
-    
-    -- Loading Spoons
-    hs.loadSpoon("MiroWindowsManager")
-    
-    -- Settings Windows Manager
-    hs.window.animationDuration = 0
-    spoon.MiroWindowsManager:bindHotkeys({
-        up =         { cmd_alt, "up" },
-        right =      { cmd_alt, "right" },
-        down =       { cmd_alt, "down" },
-        left =       { cmd_alt, "left" },
-        fullscreen = { cmd_alt, "m" }
-    }
-)
+movewindows.start = function()
+	-- Keybindings
+	local ctrl_cmd = { "cmd", "ctrl" }
+	local cmd_alt = { "cmd", "alt" }
+	local shift_ctrl_cmd = { "shift", "cmd", "ctrl" }
 
-hs.loadSpoon("WindowScreenLeftAndRight")
-spoon.WindowScreenLeftAndRight.animationDuration = 0
-spoon.WindowScreenLeftAndRight:bindHotkeys(
-    {
-        screen_left = { ctrl_cmd, "Left" },
-        screen_right= { ctrl_cmd, "Right" },
-    })
-    
+	-- Loading Spoons
+	hs.loadSpoon("MiroWindowsManager")
+
+	-- Settings Windows Manager
+	hs.window.animationDuration = 0
+	spoon.MiroWindowsManager:bindHotkeys({
+		up = { cmd_alt, "up" },
+		right = { cmd_alt, "right" },
+		down = { cmd_alt, "down" },
+		left = { cmd_alt, "left" },
+		fullscreen = { cmd_alt, "m" },
+	})
+
+	hs.loadSpoon("WindowScreenLeftAndRight")
+	spoon.WindowScreenLeftAndRight.animationDuration = 0
+	spoon.WindowScreenLeftAndRight:bindHotkeys({
+		screen_left = { ctrl_cmd, "Left" },
+		screen_right = { ctrl_cmd, "Right" },
+	})
 end
 return movewindows
